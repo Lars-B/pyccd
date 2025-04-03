869de301d8d3d27497309d2a4e2acc4b67d55c41
Lars-B <berlinglars96@gmail.com>
2025-04-03 11:27:20 -0700

    more pylint changes and docstring added

--- CCDpy/extract_transmission_trees.py
+++ CCDpy/extract_transmission_trees.py
@@ -1,3 +1,10 @@
+"""
+This module reads transmission nexus files as returned by the BREATH BEAST2 package.
+It will read the trees into ete3.Tree objects and label them with their transmission ancestry.
+
+Dependencies:
+- ete3.Tree
+"""
 import re
 import ete3
 
@@ -16,18 +23,17 @@ def read_transmission_nexus(file: str) -> list:
     """
     # re_tree returns nwk string without the root height and no ; in the end
     re_tree = re.compile("\t?tree .*=? (.*$)", flags=re.I | re.MULTILINE)
-    # Used to delete the ; and a potential branchlength of the root
+    # Used to delete the ; and a potential branch length of the root
     # name_dict = get_mapping_dict(file)  # Save tree label names in dict
 
     trees = []
-    with open(file, 'r') as f:
+    with open(file, 'r', encoding="utf-8") as f:
         for line in f:
             if re_tree.match(line):
-                # tree_string = f'{re.split(re_tree, line)[1][:re.split(re_tree, line)[1].rfind(")") + 1]};'
+                # tree_string = f'{re.split(re_tree, line)[1][:re.split(re_tree, line)[1]
+                #                                              .rfind(")") + 1]};'
                 tree_string = re.split(re_tree, line)[1]
                 pattern = r"\d*\[[^\]]*\]"
-
-                # matches = re.findall(pattern, tree_string)
 
                 counter = 0  # Initialize a counter
 
@@ -38,34 +44,19 @@ def read_transmission_nexus(file: str) -> list:
                         meta_list = split_str[1][:-1].split(
                             ",")  # deleting the ] from string and splitting all the meta data
                         matching_element = next((s for s in meta_list if "blockcount" in s), None)
+                        # variable hard code with string in line above
                         block_count = int(
-                            float(matching_element.split("=")[-1]))  # variable hard code with string in line above
+                            float(matching_element.split("=")[-1]))
                         if not split_str[0]:
                             counter += 1
-                        # todo add another / and the ancestral infector
-                        return f"%{split_str[0] if split_str[0] else f'internal{counter}'}/{block_count}%"  # making new string be pair taxa;block_count
-                    else:
-                        raise NotImplementedError("Needs to be added or debugged.")
+                        # making new string be pair taxa;block_count
+                        node_name = split_str[0] if split_str[0] else f'internal{counter}'
+                        return f"%{node_name}/{block_count}%"
+                    raise NotImplementedError("Needs to be added or debugged.")
 
                 # Replace all matches
                 new_tree_string = re.sub(pattern, replace_match, tree_string)
                 new_tree = ete3.Tree(new_tree_string, format=1)
-
-                # taxa_labels = [l.name.split("/")[0][-1:] for l in new_tree]  # get a list of all taxa labels
-                # for node in new_tree.traverse("levelorder"):
-                #     if node.is_root():
-                #         # Root as special case
-                #         transm_map[node.name] = "unknown"
-                #     else:
-                #         # non root vertex'
-                #         cur_name, cur_blockcount = node.name.replace("%", "").split("/")
-                #         cur_parent_name = node.up.name
-                #         if cur_parent_name not in transm_map:
-                #             raise AttributeError("Traversal through tree failed, parent should be in the transmission map at this point!")
-                #         if cur_blockcount == -1:
-                #             # the edge towards the current node had not transmission event
-                # # todo traverse the new tree to label the transmission history into the nodes
-                # unknown_count = 0  # counting the unknown ancestors for separation
 
 
                 # adjusting the tree to contain the blockcount label and correct node names
