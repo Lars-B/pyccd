26c6ca543964d4eb2eea9378edf4fb43d70ac620
Lars-B <berlinglars96@gmail.com>
2025-04-03 14:09:45 -0700

    deleting multiple read nexus functions, fix up linter errrors and adapt to new read in function

--- CCDpy/transmission_ccd.py
+++ CCDpy/transmission_ccd.py
@@ -4,81 +4,29 @@ import warnings
 from collections import defaultdict
 from dataclasses import dataclass
 
+import numpy as np
+from docutils.io import InputError
 
-import ete3
+from CCDpy.read_breath_nexus import read_transmission_nexus_history
 
-
-def read_transmission_nexus(file: str) -> list:
-    """
-    Function to read a nexus file that contains transmission trees.
-
-    :param file: Input file
-    :type file: str
-    :return: list of transmission trees
-    :rtype: list
-    """
-    # re_tree returns nwk string without the root height and no ; in the end
-    re_tree = re.compile("\t?tree .*=? (.*$)", flags=re.I | re.MULTILINE)
-    # Used to delete the ; and a potential branchlength of the root
-    # name_dict = get_mapping_dict(file)  # Save tree label names in dict
-
-    trees = []
-    with open(file, 'r') as f:
-        for line in f:
-            if re_tree.match(line):
-                tree_string = f'{re.split(re_tree, line)[1][:re.split(re_tree, line)[1].rfind(")") + 1]};'
-                # todo create a fully labelled tree, i.e. each node is labelled with a tuple
-                #  the tuple contains (taxa id, block count)
-                #  this should be able to contain more than just a specific information?
-
-                pattern = r"\d*\[[^\]]*\]"
-
-                # matches = re.findall(pattern, tree_string)
-
-                counter = 0  # Initialize a counter
-                def replace_match(match):
-                    nonlocal counter
-                    counter += 1
-                    split_str = match.group(0).split("[&")
-                    if len(split_str) == 2:
-                        # its a leaf edge so there is already a taxon name
-                        meta_list = split_str[1][:-1].split(",") # deleting the ] from string and splitting all the meta data
-                        matching_element = next((s for s in meta_list if "blockcount" in s), None)
-                        block_count = int(
-                            float(matching_element.split("=")[-1]))  # variable hard code with string in line above
-                        return f"%{split_str[0]}/{block_count}%"  # making new string be pair taxa;block_count
-                    elif len(split_str) == 1:
-                        # internal edge so no node label
-                        meta_list = split_str[0][:-1].split(",")  # deleting the ] from string and splitting all the meta data
-                        matching_element = next((s for s in meta_list if "blockcount" in s), None)
-                        block_count = int(
-                            float(matching_element.split("=")[-1]))  # variable hard code with string in line above
-                        # todo this naming doesn't work properly
-                        return f"%internal{counter}/{block_count}%"  # making new string be pair taxa;block_count
-                    else:
-                        raise NotImplementedError("Needs to be added or debugged.")
-                # Replace all matches
-                new_tree_string = re.sub(pattern, replace_match, tree_string)
-                trees.append(ete3.Tree(new_tree_string, format=1))
-                # trees.append(ete3.Tree(re.sub(brackets, "", tree_string), format=0))
-    return trees
 
 @dataclass(frozen=True)
-class transmission_clade:
-    '''Class to keep clades with additional transmission block information'''
+class TransmissionClade:
+    """Class to keep clades with additional transmission block information"""
     clade: frozenset
     has_block: bool
     # blockcount: int  # not sure if I want this in here or not...
+
     def __len__(self):
         return len(self.clade)
 
     def __lt__(self, other):
-        if isinstance(other, transmission_clade):
+        if isinstance(other, TransmissionClade):
             return len(self.clade) < len(other.clade)
         return NotImplemented
 
     def __gt__(self, other):
-        if isinstance(other, transmission_clade):
+        if isinstance(other, TransmissionClade):
             return len(self.clade) > len(other.clade)
         return NotImplemented
 
@@ -87,19 +35,20 @@ def get_transmission_clades(tree, blockcountmap):
     treestr = tree.write(format=8)
     clades = set()
     if not (treestr[0] == '(' and treestr[-2] == ')' and treestr[-1] == ';'):
-        # todo the above second statement is not true since there might be root info that we can ignore
-        raise Exception("Invalid tree string given! (no ';' at the end)")
+        # todo the above second statement is not true: there might be root info that we can ignore
+        raise InputError("Invalid tree string given! (no ';' at the end)")
 
     # Adding leafs separately
     leafnamepattern = r"%(-?\d+/-?\d+)%"
-    # the above regex assumes that the taxa are integers and internal nodes do not have an interger in the first part of the block.
+    # the above regex assumes that the taxa are integers and internal nodes do not have an
+    # interger in the first part of the block.
     matches = re.findall(leafnamepattern, treestr)
     for m in matches:
         taxa, blockcount = map(int, m.split("/"))
         if blockcount == -1:
-            leafclade = transmission_clade(frozenset({taxa}), False)
+            leafclade = TransmissionClade(frozenset({taxa}), False)
         else:
-            leafclade = transmission_clade(frozenset({taxa}), True)
+            leafclade = TransmissionClade(frozenset({taxa}), True)
             if leafclade in blockcountmap:
                 blockcountmap[leafclade] += blockcount
             else:
@@ -107,6 +56,7 @@ def get_transmission_clades(tree, blockcountmap):
         clades.add(leafclade)
 
     leafreplacementpattern = r"%(-?\d+)/-?\d+%"
+
     def replace_match(match):
         return match.group(1)  # Extract leafname from above pattern (the part before the slash)
 
@@ -122,18 +72,22 @@ def get_transmission_clades(tree, blockcountmap):
             opend.append(i)
         elif treestr[i] == ')':
             if not opend:
-                raise Exception("Invalid tree string given! (to many ')')")
+                raise InputError("Invalid tree string given! (to many ')')")
             # current_string = treestr[opend[-1]:i]
 
             match_interenalnodename = re.search(internalnodenamepattern, treestr[i+1:])
             if not match_interenalnodename:
                 raise ValueError("Something went wrong looking for the next internal nodes name")
-            blockcount = int(match_interenalnodename.group(0).replace("%", "").split("/")[1])
-            current_clade_set = frozenset(re.sub(re_brackets_internals, "", treestr[opend[-1]:i]).split(","))
+            blockcount = int(
+                match_interenalnodename.group(0).replace("%", "").split("/")[1]
+            )
+            current_clade_set = frozenset(
+                re.sub(re_brackets_internals, "", treestr[opend[-1]:i]).split(",")
+            )
             if blockcount == -1:
-                curclade = transmission_clade(current_clade_set, False)
+                curclade = TransmissionClade(current_clade_set, False)
             else:
-                curclade = transmission_clade(current_clade_set, True)
+                curclade = TransmissionClade(current_clade_set, True)
                 if curclade in blockcountmap:
                     blockcountmap[curclade].append(blockcount)
                 else:
@@ -141,54 +95,49 @@ def get_transmission_clades(tree, blockcountmap):
             clades.add(curclade)
             del opend[-1]
     if opend:
-        raise Exception("Invalid tree string given! (to many '(')")
+        raise InputError("Invalid tree string given! (to many '(')")
     return clades
 
 
 def get_transmission_maps(trees):
 
-    # todo keep track of the branch lengths here and return list for these attributes (make extendable)
-    #  extend the idea of blockcount map to be attribute_map{blockcoutmap: {}, branchlengths: {}, etc.}
-    #  maybe control the attributes to summarize with a list of keywords
-
     m1 = defaultdict(int)  # map for each clade how often it got sampled
     m2 = defaultdict(int)  # map for each (c1,c2) clade how often this specific relation got sampled
 
-    # todo change this to one dict with attributes!
     blockcount_map = {}
     branch_lengths_map = {}
 
-    # todo for this I will need to change the naming of nodes since I write this information into the node name...
-    # block_start_map = {}
-    # block_end_map = {}
-
-
-    for ix, t in enumerate(trees):
+    for _, t in enumerate(trees):
         for node in t.traverse("levelorder"):
             if len(node) > 1:
                 c = node.children
                 c0_leafs = set()
                 for leaf in c[0]:
-                    c0_leafs.add(int(leaf.name.replace("%", "").split("/")[0]))
+                    # c0_leafs.add(int(leaf.name.replace("%", "").split("/")[0]))
+                    c0_leafs.add(int(leaf.name))
                 c1_leafs = set()
                 for leaf in c[1]:
-                    c1_leafs.add(int(leaf.name.replace("%", "").split("/")[0]))
+                    # c1_leafs.add(int(leaf.name.replace("%", "").split("/")[0]))
+                    c1_leafs.add(int(leaf.name))
 
                 parent_clade_set = frozenset(sorted(c0_leafs.union(c1_leafs)))
 
                 if node.name:
-                    blockcount = int(node.name.replace("%", "").split("/")[1])
+                    # blockcount = int(node.name.replace("%", "").split("/")[1])
+                    assert hasattr(node, "blockcount"), ("The nodes should have the blockcount "
+                                                         "attribute!")
+                    blockcount = node.blockcount
                     if blockcount == -1:
-                        parent_clade = transmission_clade(parent_clade_set, False)
+                        parent_clade = TransmissionClade(parent_clade_set, False)
                     else:
-                        parent_clade = transmission_clade(parent_clade_set, True)
+                        parent_clade = TransmissionClade(parent_clade_set, True)
                         if parent_clade in blockcount_map:
                             blockcount_map[parent_clade].append(blockcount)
                         else:
                             blockcount_map[parent_clade] = [blockcount]
                 else:
-                    # this is the root, apprarently we don't have the name atm
-                    parent_clade = transmission_clade(parent_clade_set, False)
+                    # this is the root, apparently we don't have the name atm
+                    parent_clade = TransmissionClade(parent_clade_set, False)
 
                 # adding distance of parent clade to map of branch lengths
                 if parent_clade in branch_lengths_map:
@@ -198,10 +147,10 @@ def get_transmission_maps(trees):
 
                 m1[parent_clade] += 1
 
-                blockcount_c0 = int(c[0].name.replace("%", "").split("/")[1])
-                blockcount_c1 = int(c[1].name.replace("%", "").split("/")[1])
-                child0_clade = transmission_clade(frozenset(c0_leafs), blockcount_c0 != -1)
-                child1_clade = transmission_clade(frozenset(c1_leafs), blockcount_c1 != -1)
+                blockcount_c0 = c[0].blockcount
+                blockcount_c1 = c[1].blockcount
+                child0_clade = TransmissionClade(frozenset(c0_leafs), blockcount_c0 != -1)
+                child1_clade = TransmissionClade(frozenset(c1_leafs), blockcount_c1 != -1)
 
                 # in m2 the split clade with the lower int for taxa is entered first
                 if min(c0_leafs) < min(c1_leafs):
@@ -210,29 +159,29 @@ def get_transmission_maps(trees):
                     m2[(parent_clade, child1_clade, child0_clade)] += 1
             elif len(node) == 1:
                 # leaf node for which we need to add the blockcount to the blockcount_map
-                leaf_label, blockcount = map(int, node.name.replace("%", "").split("/"))
+                leaf_label = int(node.name)
+                blockcount = node.blockcount
 
-                leaf_clade = transmission_clade(frozenset({leaf_label}), (blockcount != -1))
-                # todo change to default dicts of lists that we don't need to ask all the ifs here?
+                leaf_clade = TransmissionClade(frozenset({leaf_label}), (blockcount != -1))
                 # adding branch length of the leaf node to the dict
                 if leaf_clade in branch_lengths_map:
                     branch_lengths_map[leaf_clade].append(node.dist)
                 else:
                     branch_lengths_map[leaf_clade] = [node.dist]
 
-                # if the clade has a block we need to keep track of the blockcount for later summarizing
+                # if the clade has a block we need to keep track of the blockcount for summaries
                 if blockcount != -1:
                     if leaf_clade in blockcount_map:
                         blockcount_map[leaf_clade].append(blockcount)
                     else:
                         blockcount_map[leaf_clade] = [blockcount]
-    # todo change return to be a single map
     return m1, m2, blockcount_map, branch_lengths_map
 
 
 def get_transmission_ccd_tree_bottom_up(m1, m2, blockcount_map, branch_lengths_map):
     seen_resolved_clades = {}
-    all_clades = sorted(list(m1.keys()), key=len)  # sorted list of all clades, small (cherries) to big
+    # sorted list of all clades, small (cherries) to big
+    all_clades = sorted(list(m1.keys()), key=len)
 
     for current_clade in all_clades:
         # the following are all triplets that represent how the current clade splits
@@ -268,7 +217,7 @@ def get_transmission_ccd_tree_bottom_up(m1, m2, blockcount_map, branch_lengths_m
                 if seen_resolved_clades[current_split[0]][0] < split_prob:
                     seen_resolved_clades[current_split[0]] = (split_prob, current_split)
                 elif seen_resolved_clades[current_split[0]][0] == split_prob:
-                    # todo add a way to make the tie breaking deterministic or otherwise controllable
+                    # todo add a way to make the tie breaking deterministic or seedable
                     warnings.warn("Tie breaking in effect.")
                     seen_resolved_clades[current_split[0]] = (split_prob, current_split)
             else:
@@ -287,24 +236,26 @@ def get_transmission_ccd_tree_bottom_up(m1, m2, blockcount_map, branch_lengths_m
             working_list.append(cur_split[1])
         if len(cur_split[2]) > 1:
             working_list.append(cur_split[2])
-    import numpy as np
     # reccursive function to write newick based on the output dict created above
+
     def recursive_nwk_split_dict(clade):
-        # todo add other summary annotations to this funciton as an option
         nonlocal output
         nonlocal blockcount_map
         nonlocal branch_lengths_map
         if len(clade) == 1:
-            return f"{next(iter(clade.clade))}[&blockcount={np.median(blockcount_map[clade]) if clade.has_block else -1}]:{np.mean(branch_lengths_map[clade])}"
-        else:
-            return (f"({recursive_nwk_split_dict(output[clade][0])},"
-                    f"{recursive_nwk_split_dict(output[clade][1])})"
-                    f"[&blockcount={np.median(blockcount_map[clade]) if clade.has_block else -1}]:{np.mean(branch_lengths_map[clade])}")
+            return (f"{next(iter(clade.clade))}"
+                    f"[&blockcount={np.median(blockcount_map[clade]) if clade.has_block else -1}]"
+                    f":{np.mean(branch_lengths_map[clade])}")
+
+        return (f"({recursive_nwk_split_dict(output[clade][0])},"
+                f"{recursive_nwk_split_dict(output[clade][1])})"
+                f"[&blockcount={np.median(blockcount_map[clade]) if clade.has_block else -1}]"
+                f":{np.mean(branch_lengths_map[clade])}")
 
     return recursive_nwk_split_dict(root_clade)
 
 
-def transmissionCCD_MAP_nexus(input_trees_file, output_tree_file, overwrite=False, burnin=0):
+def transmission_ccd_map_nexus(input_trees_file, output_tree_file, overwrite=False, burnin=0):
     """Takes input trees file and writes a tCCD1-MAP tree to the output file in Nexus format."""
     if os.path.exists(output_tree_file):
         if not overwrite:
@@ -314,21 +265,23 @@ def transmissionCCD_MAP_nexus(input_trees_file, output_tree_file, overwrite=Fals
     if not os.path.exists(input_trees_file):
         raise FileNotFoundError(f"Input trees {input_trees_file} not found.")
 
-    if not (0 <= burnin < 1):
-        raise ValueError("Burnin should be a number between 0 and 1 representing the proportion of trees to delete at the beginning of the file.")
+    if not 0 <= burnin < 1:
+        raise ValueError("Burnin should be a number between 0 and 1, representing the proportion "
+                         "of trees to delete at the beginning of the file.")
 
-    trees = read_transmission_nexus(input_trees_file)
+    trees = read_transmission_nexus_history(input_trees_file)
     trees = trees[int(0.1 * len(trees)):]  # deleting burnin from trees
     if len(trees) < 1:
         raise ValueError("Treeset is empty, reduce burning or check file.")
 
     m1, m2, blockcount_map, branch_lengths_map = get_transmission_maps(trees)
-    newick_MAP = get_transmission_ccd_tree_bottom_up(m1, m2, blockcount_map, branch_lengths_map)
+    newick_map = get_transmission_ccd_tree_bottom_up(m1, m2, blockcount_map, branch_lengths_map)
 
-    with open(input_trees_file, 'r') as infile, open(output_tree_file, 'w+') as outfile:
+    with (open(input_trees_file, 'r', encoding="UTF-8") as infile,
+          open(output_tree_file, 'w+', encoding="UTF-8") as outfile):
         for line in infile:
             if line.strip().startswith("tree "):
                 break  # Stop reading when the\ tree section starts
             outfile.write(line)
 
-        outfile.write(f"tree tCCD_MAP = {newick_MAP};\nEnd;\n")
+        outfile.write(f"tree tCCD_MAP = {newick_map};\nEnd;\n")
--- tests/test_transmission_ccd.py
+++ tests/test_transmission_ccd.py
@@ -1,4 +1,5 @@
-from CCDpy.transmissionCCD import *
+from CCDpy.read_breath_nexus import read_transmission_nexus_history
+from CCDpy.transmission_ccd import *
 from pathlib import Path
 
 
@@ -6,26 +7,26 @@ from pathlib import Path
 
 def test_read_transmission_nexus():
     test_tree_file = f"{Path(__file__).parent.absolute()}/data/Filter-roetzer40.trees"
-    trees = read_transmission_nexus(test_tree_file)
+    trees = read_transmission_nexus_history(test_tree_file, False)
     # test = get_transmission_clades(trees[1], blockcountmap)
-    m1, m2, blockcount_map = get_transmission_maps(trees)
-    get_transmission_ccd_tree_bottom_up(m1, m2, blockcount_map)
+    m1, m2, blockcount_map, branch_lengths_map = get_transmission_maps(trees)
+    get_transmission_ccd_tree_bottom_up(m1, m2, blockcount_map, branch_lengths_map)
     assert False
 
 
 def test_transmissionCCD_MAP_nexus():
     test_tree_file = f"{Path(__file__).parent.absolute()}/data/Filter-roetzer40.trees"
     output_file = f"{Path(__file__).parent.absolute()}/data/Filter-roetzer40-tCCD.tree"
-    transmissionCCD_MAP_nexus(input_trees_file=test_tree_file,
-                              output_tree_file=output_file,
-                              overwrite=True,
-                              burnin=.1)
+    transmission_ccd_map_nexus(input_trees_file=test_tree_file,
+                               output_tree_file=output_file,
+                               overwrite=True,
+                               burnin=.1)
     assert True
 
 
 def test_lazy():
     test_tree_file = f"{Path(__file__).parent.absolute()}/data/roetzer40.trees"
     output_file = f"{Path(__file__).parent.absolute()}/data/testing_tCCD.tree"
-    transmissionCCD_MAP_nexus(input_trees_file=test_tree_file,
-                              output_tree_file=output_file, overwrite=True, burnin=.1)
+    transmission_ccd_map_nexus(input_trees_file=test_tree_file,
+                               output_tree_file=output_file, overwrite=True, burnin=.1)
     assert True
