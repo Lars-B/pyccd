4b34ac592679be4c61753e405c3e6ac5bae40efe
Lars-B <berlinglars96@gmail.com>
2025-04-25 17:07:44 -0700

    clean up

--- tests/test_transmission_ccd.py
+++ tests/test_transmission_ccd.py
@@ -1,13 +1,10 @@
 """
 Tests for the transmission_ccd.py functions
 """
-from pathlib import Path
-
 import pytest
 
-import pyccd.read_nexus
-from pyccd.transmission_ccd import (TypeCCD, get_transmission_maps,
-                                    get_transmission_ccd_tree_bottom_up)
+from pyccd.transmission_ccd import TypeCCD, get_transmission_maps
+
 
 
 def test_valid_enum():
@@ -33,27 +30,3 @@ def test_get_transmission_maps_wrong_enum_conversion():
     with pytest.raises(ValueError) as e:
         get_transmission_maps(trees=[], type_str="Bla")
     assert str(e.value).startswith("Type 'Bla' not recognized.")
-
-
-def test_read_transmission_nexus():
-    """
-        WIP This will be moved to example folder shortly
-    """
-    test_tree_file = f"{Path(__file__).parent.absolute()}/data/Filter-roetzer40.trees"
-    trees = pyccd.read_nexus.read_nexus_trees(test_tree_file, breath_trees=True,
-                                              label_transm_history=False)
-    m1, m2, blockcount_map, branch_lengths_map = get_transmission_maps(trees)
-    get_transmission_ccd_tree_bottom_up(m1, m2, blockcount_map, branch_lengths_map)
-    assert True, "Just testing if nothing breaks, apparently something did..."
-
-
-def test_transmisison_ccd_hiostory():
-    """
-        WIP This will be moved to example folder shortly
-    """
-    test_tree_file = f"{Path(__file__).parent.absolute()}/data/Filter-roetzer40.trees"
-    trees = pyccd.read_nexus.read_nexus_trees(test_tree_file, breath_trees=True,
-                                              label_transm_history=True)
-    m1, m2, blockcount_map, branch_lengths_map = get_transmission_maps(trees, type_str="Ancestry")
-    tree = get_transmission_ccd_tree_bottom_up(m1, m2, blockcount_map, branch_lengths_map)
-    assert tree is not None, "Failed this example..."
