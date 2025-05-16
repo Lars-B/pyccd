deb7134c2213962cff0ba97039877287b000ff99
Lars-B <berlinglars96@gmail.com>
2025-05-16 11:51:55 -0700

    adding print to satisfy linter for now, examples are still WIP

--- examples/example_extract_transmission_trees.py
+++ examples/example_extract_transmission_trees.py
@@ -14,6 +14,7 @@ def read_transmission_nexus():
     # test_tree_file = f"{Path(__file__).parent.absolute()}/data/Filter-roetzer40.trees"
     test_tree_file = f"{Path(__file__).parent.absolute().parent}/tests/data/BREATH5taxa.trees"
     trees = read_nexus_trees(test_tree_file)
+    print(len(trees))
     # Testing how to output these trees, still WIP and unfinished
     # teststr = trees[0].write(
     #               features=["blockcount", "transm_ancest"], format_root_node=True, format=2)
@@ -27,6 +28,7 @@ def label_transmission_tree():
     tree_file = f"{Path(__file__).parent.absolute().parent}/tests/data/Filter-roetzer40.trees"
     trees = read_nexus_trees(tree_file)
     cur_tree_nwk = trees[1].write(features=["transm_ancest"], format_root_node=True, format=2)
+    print(cur_tree_nwk)
 
 
 if __name__ == '__main__':
