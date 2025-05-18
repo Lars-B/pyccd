87b85324b028a01fefcc17868fc2f141807f0090
Lars-B <berlinglars96@gmail.com>
2025-05-18 14:29:12 -0700

    minor doc config change

--- docs/conf.py
+++ docs/conf.py
@@ -34,6 +34,4 @@ autodoc_default_options = {
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-# html_theme = "classic"
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
