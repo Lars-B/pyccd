22a5f26013863d08aa39e15feaf4cc77f409a8b6
Lars-B <berlinglars96@gmail.com>
2025-11-29 12:33:52 -0800

    Adding option for both BREATH examples, some reformatting

--- examples/motivating_example.py
+++ examples/motivating_example.py
@@ -30,7 +30,8 @@ def phylgeo_example():
         )
     )
     print("Geo CCD1 map:")
-    print(geo_ccd_map_tree.write(format=5, features=["type"], format_root_node=True))
+    print(geo_ccd_map_tree.write(format=5, features=["type"],
+                                 format_root_node=True))
 
     # todo get the regular CCD and map tree too
     # call beast for this to have the summary of states too...
@@ -41,22 +42,24 @@ def phylgeo_example():
         )
     )[0]
     print("topo CCD1 map:")
-    print(beast_ccd1_tree.write(format=5, features=["type"], format_root_node=True))
-    # TODO Note that I currently have no idea how beast gets the root annotation? why is it 0+1
-    #  and why are the probabilities of 0 and 1 0.4?
+    print(beast_ccd1_tree.write(format=5, features=["type"],
+                                format_root_node=True))
 
     # todo can I reimplement the function to get all represented trees from a CCD?
     # todo implement the sample form the CCD function....
-    from brokilon.ccd.domain.phylogeography import get_all_trees_represented
-    all_trees_represented = get_all_trees_represented(geo_ccd_map)
+    # from brokilon.ccd.domain.phylogeography import get_all_trees_represented
+    # all_trees_represented = get_all_trees_represented(geo_ccd_map)
 
 
-def breath_example():
+def breath_example(basic=False):
+    trees_path = (
+        "data/basic_motivating_example_breath.trees" if basic else
+        "data/motivating_example_breath.trees"
+    )
     posterior = read_breath_nexus(
         os.path.join(
             Path(__file__).parent.absolute(),
-            # "data/basic_motivating_example_breath.trees"
-            "data/motivating_example_breath.trees"
+            trees_path
         ),
         parse_taxon_map=False
     )
@@ -66,8 +69,9 @@ def breath_example():
     from brokilon.ccd.domain.transmission import get_transmission_maps, \
         get_transmission_ccd_tree_bottom_up
 
-    m1, m2, blockcount_map, branch_lengths_map = get_transmission_maps(posterior,
-                                                                       type_str="Ancestry")
+    m1, m2, blockcount_map, branch_lengths_map = get_transmission_maps(
+        posterior,
+        type_str="Ancestry")
     newick_map = get_transmission_ccd_tree_bottom_up(
         m1,
         m2,
@@ -80,11 +84,14 @@ def breath_example():
     print(newick_map)
     print("-------")
 
+    beast_ccd_tree_file = (
+        "data/basic_motivating_breath_ccd1_beast.tree" if basic else
+        "data/motivating_breath_ccd1_beast.tree"
+    )
     beast_ccd1_tree = read_breath_nexus(
         os.path.join(
             Path(__file__).parent.absolute(),
-            # "data/basic_motivating_breath_ccd1_beast.tree"
-            "data/motivating_breath_ccd1_beast.tree"
+            beast_ccd_tree_file
         ),
         False
     )[0]
@@ -100,4 +107,5 @@ def breath_example():
 
 if __name__ == '__main__':
     # phylgeo_example()
-    breath_example()
+    # breath_example()
+    breath_example(basic=True)
