7dcc800820dfa4647e31e5351a566883ab236b3c
Lars-B <berlinglars96@gmail.com>
2026-03-31 14:43:13 -0700

    sranges testing and devel code, WIP

--- examples/sranges.py
+++ examples/sranges.py
@@ -9,17 +9,22 @@ test_mcc_tree_file = (f"{Path(__file__).parent.absolute().parent}"
 
 from brokilon.core import read_nexus_trees
 
-trees, map = read_nexus_trees(test_mcc_tree_file, parse_taxon_map=True)
+trees, map = read_nexus_trees(test_tree_file, parse_taxon_map=True)
 
 from brokilon.ccd.domain.sranges import sranges
 
 clade_counts, clade_split_counts = sranges.get_sranges_map(trees, map)
 
+taxon_map = map
+reverse_taxon_map = {value: key for key, value in taxon_map.items()}
 
-for c in clade_counts.keys():
-    print(c)
+map_tree = sranges.get_sranges_map_tree(clade_counts, clade_split_counts, taxon_map, reverse_taxon_map)
 
-for c in clade_split_counts.keys():
-    print(c)
+
+# for c in clade_counts.keys():
+#     print(c)
+#
+# for c in clade_split_counts.keys():
+#     print(c)
 # print(clade_counts)
 # print(clade_split_counts)
